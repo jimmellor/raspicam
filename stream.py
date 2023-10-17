f8594b3d2171c981e0736beef078aeb2158ad5ce
Jim Mellor <jim.mellor@me.com>
2023-10-17 18:12:12 +0100

    fix create_video_configuration

--- stream.py
+++ stream.py
@@ -3,7 +3,7 @@ from picamera2 import Picamera2
 import time
 
 picam2 = Picamera2()
-video_config = picam2.video_configuration({"size": (640, 480)})
+video_config = picam2.create_video_configuration({"size": (640, 480)})
 picam2.configure(video_config)
 encoder = H264Encoder(bitrate=1000000, repeat=True, iperiod=15)
 output = FfmpegOutput("-f hls -hls_time 4 -hls_list_size 5 -hls_flags delete_segments -hls_allow_cache 0 stream.m3u8")
